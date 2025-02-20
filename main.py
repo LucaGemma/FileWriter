4fa86f84d5d105f58aee55e32a6f53cc28f8136e
LucaGemma <luca.gemma.91@gmail.com>
2025-02-20 10:52:49 +0100

    Added print to file to main.py
    Added instructions to print a string to the 'output.txt' file.

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ nome_completo = nome + " " + cognome
 print(f"{nome = }")
 print(f"{cognome = }")
 print(f"{nome_completo = }")
+
+nome_file = "output.txt"
+stringa_da_scrivere = "Hello world"
+
+# Aggiungo la stringa "Hello world" al file output.txt
+with open(nome_file, mode = "a") as file:
+    file.write(stringa_da_scrivere)
+
+print(f"\nStringa '{stringa_da_scrivere}' scritta sul file '{nome_file}' correttamente.")
