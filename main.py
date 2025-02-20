1873442c8946421450e82e446e36014f8868a5a1
LucaGemma <luca.gemma.91@gmail.com>
2025-02-20 11:19:14 +0100

    fixed main.py?
    I don't even have idea of what I am doing...

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ print(f"{cognome = }")
 print(f"{nome_completo = }")
 
 nome_file = "output.txt"
-stringa_da_scrivere = "Hello world"
+stringa_da_scrivere = "Hello world!\n"
 
 # Aggiungo la stringa "Hello world" al file output.txt
 with open(nome_file, mode = "a") as file:
-    file.write(stringa_da_scrivere)
+    file.writ(stringa_da_scrivere)
 
-print(f"\nStringa '{stringa_da_scrivere}' scritta sul file '{nome_file}' correttamente.")
+print(f"\nStringa '{stringa_da_scrivere}' scritta sul file '{nome_file}' correttament.")
